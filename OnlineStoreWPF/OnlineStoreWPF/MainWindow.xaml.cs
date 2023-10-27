7b4c22af0aa938552ceaf1fc78228681357be593
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2023-10-27 11:51:00 +0400

    Add files via upload

---
+++ OnlineStoreWPF/OnlineStoreWPF/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace OnlineStoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        public ObservableCollection<Product> TempProducts { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Products = new ObservableCollection<Product>
+            {
+                new Product()
+                {
+                    Name = "Bread",
+                    Price = 0.75,
+                    ImagePath = @"Images\BineCorek.png",
+                },
+                new Product()
+                {
+                    Name = "Cola",
+                    Price = 1.5,
+                    ImagePath = @"Images\Cola.png",
+                },
+                new Product()
+                {
+                    Name = "Fanta",
+                    Price = 1.5,
+                    ImagePath = @"Images\Fanta.png",
+                },
+                new Product()
+                {
+                    Name = "Pepsi",
+                    Price = 1.5,
+                    ImagePath = @"Images\Pepsi-bottle.png",
+                },
+                new Product()
+                {
+                    Name = "Potato",
+                    Price = 0.75,
+                    ImagePath = @"Images\Potato.png",
+                },
+                new Product()
+                {
+                    Name = "Lays",
+                    Price = 1.8,
+                    ImagePath = @"Images\Lays.png",
+                },
+                new Product()
+                {
+                    Name = "Tomato",
+                    Price = 1.8,
+                    ImagePath = @"Images\Tomato.png"
+                }
+            };
+            TempProducts = Products;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void Image_Exit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Close();
+
+        private void Image_FullScreen_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => WindowState = WindowState.Maximized;
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton tgbtn)
+            {
+                if (tgbtn.IsChecked == true)
+                {
+                    brdSearch.Width = 300;
+                    brdSearch.Margin = new Thickness(0, 20, 40, 20);
+                    txbSearch.FontSize = 16;
+                    WindowState = WindowState.Maximized;
+                    imgScreen.Source = new BitmapImage(new Uri(@"Images\shrink_64px.png", UriKind.RelativeOrAbsolute));
+                    imgScreen.ToolTip = "Shrink screen";
+                    
+
+                }
+                else
+                {
+                    brdSearch.Width = 200;
+                    brdSearch.Margin = new Thickness(0, 5, 15, 5);
+                    txbSearch.FontSize = 12;
+                    WindowState = WindowState.Normal;
+                    imgScreen.Source = new BitmapImage(new Uri(@"Images\fullscreen_64px.png", UriKind.RelativeOrAbsolute));
+                    imgScreen.ToolTip = "Full screen";
+                }
+            }
+        }
+
+        private void TextBoxSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txbSearch.Text == "Search")
+            {
+                txbSearch.Text = "";
+                txbSearch.Foreground = Brushes.Black;
+                txbSearch.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void TextBoxSearch_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txbSearch.Text))
+            {
+                txbSearch.Text = "Search";
+                txbSearch.Foreground = Brushes.Silver;
+                txbSearch.FontWeight = FontWeights.SemiBold;
+                Products = TempProducts;
+            }
+        }
+
+        private void LbProduct_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ProductEditWindow editWindow = new ProductEditWindow();
+            editWindow.Product = lbProduct.SelectedItem as Product;
+            editWindow.ShowDialog();
+
+        }
+
+        private void Image_Add_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ProductEditWindow editWindow = new ProductEditWindow();
+            editWindow.btnImgChnOrAdd.Content = "Image add";
+            editWindow.btnAddOrDeleteProduct.Background = Brushes.LightSkyBlue;
+            editWindow.btnAddOrDeleteProduct.Content = "Add";
+            editWindow.ShowDialog();
+        }
+
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txbSearch.Text.Length != 0 && txbSearch.Text != "Search")
+            {
+                var searchText = txbSearch.Text.ToLower();
+                List<Product> productsList = new List<Product>();
+                productsList = TempProducts.Where(p => p.Name.ToLower().StartsWith(searchText)).ToList();
+                ObservableCollection<Product> newList = new ObservableCollection<Product>(productsList);
+                Products = newList;
+                lbProduct.ItemsSource = newList;
+
+            }
+            else if (txbSearch.Text.Length == 0)
+            {
+                Products = TempProducts;
+                lbProduct.ItemsSource = Products;
+            }
+        }
+    }
+}
---
+++ OnlineStoreWPF/OnlineStoreWPF/ProductEditWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+
+namespace OnlineStoreWPF
+{
+    /// <summary>
+    /// Interaction logic for ProductEditWindow.xaml
+    /// </summary>
+    public partial class ProductEditWindow : Window, INotifyPropertyChanged
+    {
+        private Product product;
+
+        public Product Product
+        {
+            get { return product; }
+            set
+            {
+                product = value;
+                OnPropertyRaised();
+            }
+        }
+
+        public ProductEditWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        private void Button_AddOrDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnAddOrDeleteProduct.Content.ToString() == "Add")
+            {
+                if (string.IsNullOrWhiteSpace(txbProdName.Text) || string.IsNullOrWhiteSpace(txbProdPrice.Text) || txbProdPrice.Text == "0" || imgSelectedProduct.Source == null)
+                {
+                    MessageBox.Show("Please fill in the information!");
+
+                }
+                else
+                {
+                    Product.Name = txbProdName.Text;
+                    Product.Price = double.Parse(txbProdPrice.Text);
+                    ((MainWindow)Application.Current.MainWindow).Products.Add(Product);
+                    Close();
+
+                }
+            }
+            else
+            {
+                ((MainWindow)Application.Current.MainWindow).TempProducts.Remove(Product);
+                ((MainWindow)Application.Current.MainWindow).Products.Remove(Product);
+                Close();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyRaised([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+        private void Button_ImageChange_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Select a picture";
+            openFile.Multiselect = false;
+            openFile.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg";
+            if (openFile.ShowDialog() == true)
+            {
+                imgSelectedProduct.Source = new BitmapImage(new Uri(openFile.FileName));
+                if (Product == null)
+                {
+                    Product = new Product()
+                    {
+                        Name = txbProdName.Text,
+                        Price = double.Parse(txbProdPrice.Text)
+                    };
+                }
+                Product.ImagePath = openFile.FileName;
+            }
+        }
+    }
+}
